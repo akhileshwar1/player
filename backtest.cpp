9a4beeca9e431b8e258cd5b43c6d6552dfe89dc7
Akhil Kandi <akhilprm999@gmail.com>
2026-04-01 20:37:59 +0530

    Backtest logic done

--- backtest.cpp
+++ backtest.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdint.h>
 
 #define MAX_EVENTS 10 
 
@@ -47,7 +48,7 @@ typedef struct
     real64 timeToRefresh;
     real64 buyPressure;
     real64 sellPressure;
-    timespec lastTime;
+    uint64 lastTime;
     bool isOpen;
     bool AreEventsApplied;
     bool isSnapshot;
@@ -62,7 +63,7 @@ LoadTradeEvent(Trade_event *trade, char *line, State *state)
     int i = 0;
     while (token != NULL)
     {
-        printf("field : %s\n", token);
+        // printf("field : %s\n", token);
         if (i == 0)
         {
             trade->id = (uint64)atoi(token);
@@ -102,7 +103,33 @@ LoadTradeEvent(Trade_event *trade, char *line, State *state)
             printf("Buy pressure added to %f\n", state->buyPressure);
         }
         token = strtok(NULL, ",");
+        i++;
     }
+}
+
+void
+PrintTradeState(State *state)
+{
+    printf("FORCES===================\n");
+    printf("START PRICE %f\n", state->startPrice); 
+    printf("SELL PRESSURE %f\n", state->sellPressure); 
+    printf("BUY PRESSURE %f\n", state->buyPressure); 
+}
+
+void
+BufferTradeEvent(Trade_event tradeEvent, Trade_events_buffer *tradeEventsBuffer)
+{
+    uint16 currentWriteIndex = tradeEventsBuffer->currentWriteIndex;
+    uint16 size = tradeEventsBuffer->size;
+    if (currentWriteIndex >= size)
+    {
+        tradeEventsBuffer->currentWriteIndex = currentWriteIndex % size;
+    }
+    tradeEventsBuffer->buffer[tradeEventsBuffer->currentWriteIndex] = tradeEvent;
+    printf("Buffered event at index %u\n",
+           tradeEventsBuffer->currentWriteIndex);
+    tradeEventsBuffer->currentWriteIndex++;
+    tradeEventsBuffer->eventCount++;
 }
 
 int
@@ -114,9 +141,21 @@ main()
         printf("Couldn't open file\n");
         return -1;
     }
+    FILE *outputFile = fopen("output.txt", "w");
+    if (outputFile == NULL)
+    {
+        printf("Couldn't open file\n");
+        return -1;
+    }
 
     char line[1024];
     State state = {};
+    state.TradeEventsBuffer.size = MAX_EVENTS;
+    state.TradeEventsBuffer.currentWriteIndex = 0;
+    state.buyPressure = 0.0;
+    state.sellPressure = 0.0;
+    state.timeToRefresh = 30 * 60 * 1000;
+
     while (fgets(line, sizeof(line), myFile) != NULL)
     {
         char *tmp = strchr(line, '\n');
@@ -124,6 +163,192 @@ main()
 
         Trade_event trade = {};
         LoadTradeEvent(&trade, line, &state);
+        BufferTradeEvent(trade, &(state.TradeEventsBuffer));
+        real64 buyPressure = state.buyPressure;
+        real64 sellPressure = state.sellPressure;
+        real64 lastPrice = state. 
+            TradeEventsBuffer.
+            buffer[MAX_EVENTS - 1].price;
+        if ( !state.isOpen &&
+            lastPrice != 0.0)
+        {
+            uint64 endTime = trade.time;
+            real64 timeElapsedMS = endTime - state.lastTime;
+
+            if (timeElapsedMS > state.timeToRefresh)
+            {
+                state.startPrice = lastPrice;
+                state.lastTime = endTime;
+                state.buyPressure = 0.0;
+                state.sellPressure = 0.0;
+            }
+
+            printf("start price is %f\n", state.startPrice);
+
+            if (abs((lastPrice - state.startPrice)) < 1)
+            {
+                printf("Guilty! There is no price movement\n");
+            }
+            else
+            {
+                char body[300];
+                if (lastPrice > state.startPrice)
+                {
+                    if (buyPressure < 2 * sellPressure)
+                    {
+                        printf("Guilty! Not enough pressure on buy side\n");
+                    }
+                    else if (timeElapsedMS < 25 * 60 * 1000)
+                    {
+                        printf("Guilty! Too fast, need real slow and steady!\n");
+                    }
+                    else
+                {
+                        printf("opening the position after %f at lastPrice %f\n",
+                               timeElapsedMS,
+                               lastPrice);
+                        // make the order call.
+                        sprintf(body, "symbol=%s&side=%s&type=%s&quantity=%f&timestamp=%lu",
+                                "SOLUSDT",
+                                "BUY",
+                                "MARKET",
+                                0.1,
+                                endTime);
+                        printf("body is %s, api key is %s\n", body, getenv("API_KEY"));
+                        state.posnType = LONG;
+                        // BinanceMakeOrder(curl, body);
+                        fputs(strcat(body, "\n"), outputFile);
+                        state.timeToClose = timeElapsedMS;
+                        state.lastTime = endTime;
+                        state.isOpen = true;
+                        state.buyPressure = 0.0;
+                        state.sellPressure = 0.0;
+                    }
+                }
+                else
+                {
+                    if (sellPressure < 2 * buyPressure)
+                    {
+                        printf("Guilty! Not enough pressure on sell side\n");
+                    }
+                    else if (timeElapsedMS < 25 * 60 * 1000)
+                    {
+                        printf("Guilty! Too fast, need real slow and steady!\n");
+                    }
+                    else
+                    {
+                        printf("opening the position after %f at lastPrice %f\n",
+                               timeElapsedMS,
+                               lastPrice);
+                        // make the order call.
+                        sprintf(body, "symbol=%s&side=%s&type=%s&quantity=%f&timestamp=%lu",
+                                "SOLUSDT",
+                                "SELL",
+                                "MARKET",
+                                0.1,
+                                endTime);
+                        printf("body is %s, api key is %s\n", body, getenv("API_KEY"));
+                        state.posnType = SHORT;
+                        // BinanceMakeOrder(curl, body);
+                        fputs(strcat(body, "\n"), outputFile);
+                        state.timeToClose = timeElapsedMS;
+                        state.lastTime = endTime;
+                        state.isOpen = true;
+                        state.buyPressure = 0.0;
+                        state.sellPressure = 0.0;
+                    }
+                }
+            }
+        }
+        // close the position.
+        else if (state.isOpen &&
+            lastPrice != 0.0)
+        {
+
+            uint64 endTime = trade.time;
+            real64 timeElapsedMS = endTime - state.lastTime;
+            Posn_type posnType = state.posnType;
+            printf("position is open, remaining time %f\n",
+                   state.timeToClose - timeElapsedMS);
+
+            if (timeElapsedMS < state.timeToClose)
+            {
+                printf("Guilty! No need to close, time not out\n");
+            }
+            else if (posnType == LONG && !(sellPressure > 2 * buyPressure))
+            {
+                printf("Guilty! No need to close, long pressure not reversed, so Load up!\n");
+                printf("Loading the position at lastPrice %f\n", lastPrice);
+                char body[300];
+                sprintf(body, "symbol=%s&side=%s&type=%s&quantity=%f&timestamp=%lu",
+                        "SOLUSDT",
+                        "SELL",
+                        "MARKET",
+                        0.1,
+                        endTime);
+                printf("body is %s, api key is %s\n", body, getenv("API_KEY"));
+                // BinanceMakeOrder(curl, body);
+                fputs(strcat(body, "\n"), outputFile);
+                state.startPrice = lastPrice;
+                state.lastTime = endTime;
+                state.buyPressure = 0.0;
+                state.sellPressure = 0.0;
+                state.timeToClose *= 2;
+            }
+            else if (posnType == SHORT && !(buyPressure > 2 * sellPressure))
+            {
+                printf("Guilty! No need to close, short pressure not reversed, so Load up!\n");
+                printf("Loading the position at lastPrice %f\n", lastPrice);
+                char body[300];
+                sprintf(body, "symbol=%s&side=%s&type=%s&quantity=%f&timestamp=%lu",
+                        "SOLUSDT",
+                        "SELL",
+                        "MARKET",
+                        0.1,
+                        endTime);
+                printf("body is %s, api key is %s\n", body, getenv("API_KEY"));
+                // BinanceMakeOrder(curl, body);
+                fputs(strcat(body, "\n"), outputFile);
+                state.startPrice = lastPrice;
+                state.lastTime = endTime;
+                state.buyPressure = 0.0;
+                state.sellPressure = 0.0;
+                state.timeToClose *= 2;
+            }
+            else // will only close now when the pressure's have reversed.
+        {
+                printf("closing the position at lastPrice %f\n", lastPrice);
+                char body[300];
+                if (posnType == LONG)
+                {
+                    sprintf(body, "symbol=%s&side=%s&type=%s&quantity=%f&timestamp=%lu",
+                            "SOLUSDT",
+                            "SELL",
+                            "MARKET",
+                            0.1,
+                            endTime);
+                }
+                else if (posnType == SHORT)
+                {
+                    sprintf(body, "symbol=%s&side=%s&type=%s&quantity=%f&timestamp=%lu",
+                            "SOLUSDT",
+                            "BUY",
+                            "MARKET",
+                            0.1,
+                            endTime);
+                }
+                printf("body is %s, api key is %s\n", body, getenv("API_KEY"));
+                // BinanceMakeOrder(curl, body);
+                fputs(strcat(body, "\n"), outputFile);
+                state.posnType = ZERO;
+                state.isOpen = false;
+                state.startPrice = lastPrice;
+                state.lastTime = endTime;
+                state.buyPressure = 0.0;
+                state.sellPressure = 0.0;
+            }
+        }
+        PrintTradeState(&state);
     }
 
     fclose(myFile);
