b14c9f2e6b88a63e4648b81400cb2ddef16f67da
Akhil Kandi <akhilprm999@gmail.com>
2026-04-13 15:24:56 +0530

    Detect mem leaks using asan

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # while true; do
+    export ASAN_OPTIONS=detect_leaks=1
     env $(cat .env | xargs) ./player.out >> bot.log 2>&1
     # echo "Bot crashed. Restarting..."
     # sleep 1
